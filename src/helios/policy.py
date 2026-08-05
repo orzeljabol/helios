e932a6597ff840e09b5ee608d070bbe06a80fa8d
orzeljabol <zebrowskibartek7@gmail.com>
2026-08-05 09:11:27 +0200

    feat: add single worker scheduling policy

---
+++ src/helios/policy.py
@@ -0,0 +1,8 @@
+from helios.worker import MockWorker
+
+
+class SingleWorkerPolicy:
+    def __init__(self, worker: MockWorker) -> None:
+        self.worker = worker
+    def select_worker(self) -> MockWorker:
+        return self.worker
---
+++ tests/test_policy.py
@@ -0,0 +1,17 @@
+from helios.policy import SingleWorkerPolicy
+from helios.worker import MockWorker
+
+
+def test_select_worker_returns_configured_worker() -> None:
+    worker = MockWorker(
+        worker_id = "worker-1",
+        base_latency_ms = 5,
+        capacity = 1,
+        healthy = True
+    )
+
+    policy = SingleWorkerPolicy(worker)
+
+    selected = policy.select_worker()
+
+    assert selected is worker
